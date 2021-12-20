41a5057c911ad0fb8f11428f07e62760f740630c
osmanforhad <osmanforhad93@gmail.com>
2021-12-20 10:16:57 +0600

    working with laravel url concept

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -1,5 +1,9 @@
 <h4>Welcome, {{ $name }}</h4>
 
+<p> Previous Url: {{ url()->previous() }} </p>
+
+<p> Current Url: {{ url()->current() }} </p>
+
 <form action="{{ route('teacher.store') }}" method="POST">
     @csrf
     <input type="text" name="name" placeholder="write your name">
