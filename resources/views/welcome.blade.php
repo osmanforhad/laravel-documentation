b0c9f22eafa13f7217fe745c8152c6e41c526842
osmanforhad <osmanforhad93@gmail.com>
2021-12-25 11:07:14 +0600

    working with breeze auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <!-- @if (Route::has('login'))
+            @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
@@ -34,20 +34,28 @@
                         @endif
                     @endauth
                 </div>
-            @endif -->
+            @endif
             <h1>This is my home page</h1>
+            <!-- @php
+            if(Auth::check()) {
+                echo "Yes Loggedin!!";
+            }
+            @endphp -->
+            @if(Auth::check())
+            <b>Yes Loggedin</b>
+            @endif
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <ul class="list">
-                        <li class="list-item"><a href="{{ url('about') }}">About</a></li>
+                        <!-- <li class="list-item"><a href="{{ url('about') }}">About</a></li>
                         <li class="list-item"><a href="{{ url('profile') }}">Profile</a></li>
-                        <li class="list-item"><a href="{{ URL::to('portfolio') }}">Portfolio</a></li>
+                        <li class="list-item"><a href="{{ URL::to('portfolio') }}">Portfolio</a></li> -->
                         <!-- example of Name Route -->
-                        <li class="list-item"><a href="{{ route('contact.us') }}">Contact</a></li>
+                        <!-- <li class="list-item"><a href="{{ route('contact.us') }}">Contact</a></li>
                         <li class="list-item"><a href="{{ route('our.team') }}">Team</a></li>
-                        <li class="list-item"><a href="{{ route('register-user') }}">Register user</a></li>
+                        <li class="list-item"><a href="{{ route('register-user') }}">Register user</a></li> -->
                         <!-- Example of Route Hashing -->
-                        <li class="list-item"><a href="{{ route('user-location') }}">Location</a></li>
+                        <!-- <li class="list-item"><a href="{{ route('user-location') }}">Location</a></li> -->
                     </ul>
                 </div>
 
--- routes/web.php
+++ routes/web.php
@@ -8,20 +8,35 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Fresh Route
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/laravel', function () {
+    return view('zila');
+})->middleware('auth');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+/*
+|--------------------------------------------------------------------------
+| Basis Testing Routes
+|--------------------------------------------------------------------------
+|
+*/
 
 Route::get('/about', function () {
     return view('about');
@@ -77,16 +92,11 @@ Route::get('/testing', InvokableController::class);
 
 Route::get('/dist', [FirstController::class, 'dist'])->name('dist')->middleware('country');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 //Middlware Route
 Route::get('/country', function() {
     return view('country');
 })->middleware('country');
 
-require __DIR__.'/auth.php';
 
 //__Route for Session__//
 Route::get('/session-test', function(Request $request) {
